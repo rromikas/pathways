57090ba4f0b1d303e2f85e00c7e90b5909996d72
rromikas <rromikas@gmail.com>
2021-03-25 03:10:23 +0200

    ok

--- src/index.js
+++ src/index.js
@@ -4,16 +4,14 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import ServiceWorkerWrapper from "./serviceWorkerWrapper"
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <ServiceWorkerWrapper/>
       <App />
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
 
-// serviceWorkerRegistration.register();
+serviceWorkerRegistration.register();
